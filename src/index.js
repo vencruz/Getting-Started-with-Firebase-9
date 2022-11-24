da8f7772e9dc68cb03c9c22fc7ee539a29f61cf6
Venneth Cruz <mvcruz@tastradesoft.com>
2022-11-24 17:01:47 +0800

    feat: setup firestore and retrieve data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyByZzKFmNZJMC2JbaEb0TfYv5AZrAmI-4k",
   authDomain: "fir-practice-25d0b.firebaseapp.com",
@@ -6,3 +9,23 @@ const firebaseConfig = {
   messagingSenderId: "945685232567",
   appId: "1:945685232567:web:61b24989b2c9b07861c632",
 };
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const colRef = collection(db, "books");
+
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log({ err });
+  });
