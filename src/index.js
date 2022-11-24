142589e8522cee21eea5b479d30258c321a2f5d4
Venneth Cruz <mvcruz@tastradesoft.com>
2022-11-24 16:30:18 +0800

    feat: added firebase config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello from index.js')
+const firebaseConfig = {
+  apiKey: "AIzaSyByZzKFmNZJMC2JbaEb0TfYv5AZrAmI-4k",
+  authDomain: "fir-practice-25d0b.firebaseapp.com",
+  projectId: "fir-practice-25d0b",
+  storageBucket: "fir-practice-25d0b.appspot.com",
+  messagingSenderId: "945685232567",
+  appId: "1:945685232567:web:61b24989b2c9b07861c632",
+};
